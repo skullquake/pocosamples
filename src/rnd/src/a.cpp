521112a7fc52d75912af9ea00eb5b48e2ac1d597
skullquake <ockert8080@gmail.com>
2019-11-06 00:45:41 -0800

    .

---
+++ src/rnd/src/a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<Poco/Random.h>
+#include<Poco/RandomStream.h>
+int main(int argc, char** argv){
+	Poco::Random rnd;
+	rnd.seed();
+	std::cout<<rnd.next()<<std::endl;
+	std::cout<<rnd.next(10)<<std::endl;
+	std::cout<<rnd.nextChar()<<std::endl;
+	std::cout<<rnd.nextBool()<<std::endl;
+	std::cout<<rnd.nextDouble()<<std::endl;
+	Poco::RandomInputStream ris;
+	while(true){
+		std::string s;
+		ris>>s;
+		std::cout<<s<<std::endl;
+	}
+	return 0;
+}
